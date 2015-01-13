605b3e8cc27e24ccd90db275e2b744cb4b8ed9f9
Glen Henry <spritewrench@hotmail.com>
2015-01-12 21:39:22 -0500

    some folder changes

--- circle.js
+++
@@ -1,5 +0,0 @@
-              var angle = (this.player.speed) * (Math.PI/180); // Convert to radians
-              var rotatedX = Math.cos(angle) * (this.player.wep.x - this.player.x) - Math.sin(angle) * (this.player.wep.y-this.player.y) + this.player.x;
-              var rotatedY = Math.sin(angle) * (this.player.wep.x - this.player.x) + Math.cos(angle) * (this.player.wep.y - this.player.y) + this.player.y;
-              this.player.wep.x = rotatedX;
-              this.player.wep.y = rotatedY          
